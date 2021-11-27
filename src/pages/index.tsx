8162ace9912662c016c2d6a47c47d6dc8ef95e0f
tomheaton <thomas.m.heaton@gmail.com>
2021-11-27 04:18:08 +0000

    more auth work

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,96 +8,98 @@ const Home: NextPage = () => {
 
     const router = useRouter();
 
+    const { user, error, isLoading } = useUser();
+
     const handleBegin = async () => {
         await router.push("/join");
     }
 
-    const { user, error, isLoading } = useUser();
-
-    if (isLoading) return <div>Loading...</div>;
-    if (error) return <div>{error.message}</div>;
-
-    if (user) {
+    if (isLoading) {
         return (
-            <div>
-                Welcome {user.name}! <a href={"/api/auth/logout"}>Logout</a>
-            </div>
+            <div>Loading...</div>
+        );
+    }
+
+    if (error) {
+        return (
+            <div>{error.message}</div>
         );
     }
 
     return (
-        <div>
-            lol
-            <a href={"/api/auth/login"}>Login</a>
+        <div className={styles.container}>
+            <Head>
+                <title>Lana</title>
+                <meta name="description" content="Lana - A place to find where you belong." />
+                <link rel="icon" href={"/favicon.ico"} />
+            </Head>
+
+            <nav className={styles.navbar}>
+                <div className={styles.inner}>
+                    {user ? (
+                        <div>
+                            Welcome {user.name}! <a href={"/api/auth/logout"}>Logout</a>
+                        </div>
+                    ) : (
+                        <div>
+                            <a href={"/api/auth/login"}>Login</a>
+                        </div>
+                    )}
+                </div>
+            </nav>
+
+            <main className={styles.main}>
+                <h1 className={styles.title}>
+                    lana 2
+                </h1>
+                <p className={styles.description}>
+                    finding where you belong...
+                </p>
+                <div className={styles.buttonWrapper}>
+                    <button className={"btn"} onClick={handleBegin}>
+                        begin
+                    </button>
+                </div>
+            </main>
+
+            <main className={styles.main}>
+                <h1 className={styles.title}>
+                    lana 3
+                </h1>
+                <p className={styles.description}>
+                    finding where you belong...
+                </p>
+                <div className={styles.buttonWrapper}>
+                    <button className={"btn"} onClick={handleBegin}>
+                        begin
+                    </button>
+                </div>
+            </main>
+
+            <main className={styles.main}>
+                <h1 className={styles.title}>
+                    lana 4
+                </h1>
+                <p className={styles.description}>
+                    finding where you belong...
+                </p>
+                <div className={styles.buttonWrapper}>
+                    <button className={"btn"} onClick={handleBegin}>
+                        begin
+                    </button>
+                </div>
+            </main>
+
+            {/*<footer className={styles.footer}>
+                <a href="https://cognisess.com" target="_blank" rel="noopener noreferrer" >
+                    Powered by{' '}
+                    <span className={styles.logo}>
+                        <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+                    </span>
+                </a>
+            </footer>*/}
         </div>
     );
-
-    // return (
-    //     <div className={styles.container}>
-    //         <Head>
-    //             <title>Lana</title>
-    //             <meta name="description" content="Lana - A place to find where you belong." />
-    //             <link rel="icon" href={"/favicon.ico"} />
-    //         </Head>
-    //
-    //         <nav className={styles.navbar}>
-    //             <div className={styles.inner}>
-    //                 <p>Login</p>
-    //             </div>
-    //         </nav>
-    //
-    //         <main className={styles.main}>
-    //             <h1 className={styles.title}>
-    //                 lana
-    //             </h1>
-    //             <p className={styles.description}>
-    //                 finding where you belong...
-    //             </p>
-    //             <div className={styles.buttonWrapper}>
-    //                 <button className={"btn"} onClick={handleBegin}>
-    //                     begin
-    //                 </button>
-    //             </div>
-    //         </main>
-    //
-    //         <main className={styles.main}>
-    //             <h1 className={styles.title}>
-    //                 lana
-    //             </h1>
-    //             <p className={styles.description}>
-    //                 finding where you belong...
-    //             </p>
-    //             <div className={styles.buttonWrapper}>
-    //                 <button className={"btn"} onClick={handleBegin}>
-    //                     begin
-    //                 </button>
-    //             </div>
-    //         </main>
-    //
-    //         <main className={styles.main}>
-    //             <h1 className={styles.title}>
-    //                 lana
-    //             </h1>
-    //             <p className={styles.description}>
-    //                 finding where you belong...
-    //             </p>
-    //             <div className={styles.buttonWrapper}>
-    //                 <button className={"btn"} onClick={handleBegin}>
-    //                     begin
-    //                 </button>
-    //             </div>
-    //         </main>
-    //
-    //         {/*<footer className={styles.footer}>
-    //             <a href="https://cognisess.com" target="_blank" rel="noopener noreferrer" >
-    //                 Powered by{' '}
-    //                 <span className={styles.logo}>
-    //                     <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-    //                 </span>
-    //             </a>
-    //         </footer>*/}
-    //     </div>
-    // );
 }
 
 export default Home;
--- src/pages/user/[username].tsx
+++ src/pages/user/[username].tsx
@@ -1,9 +1,12 @@
 import {NextPage} from "next";
 import {useUser} from "@auth0/nextjs-auth0";
+import {useRouter} from "next/router";
 
-const UserPage: NextPage = () => {
+const UserPage = () => {
 
-    const { user, error, isLoading } = useUser();
+    const router = useRouter();
+
+    const {user, error, isLoading} = useUser();
 
     if (isLoading) {
         return (
@@ -20,20 +23,14 @@ const UserPage: NextPage = () => {
     if (user) {
         return (
             <div>
-                <img src={user.picture ? user.picture : "./icon-192x192.png"} alt={user.name ? user.name : "default"} />
+                <img src={user.picture ? user.picture : "./icon-192x192.png"} alt={user.name ? user.name : "default"}/>
                 <h2>{user.name}</h2>
                 <p>{user.email}</p>
             </div>
         );
     }
 
-    return (
-        <div>
-            <h1>
-                no user
-            </h1>
-        </div>
-    );
+    router.push("/");
 }
 
 export default UserPage;
